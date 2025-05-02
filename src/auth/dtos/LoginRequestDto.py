087236aecdf54b4ced37f45fa05535adcd8f50f5
Shahariar Kabir <maask5534@gmail.com>
2025-05-02 16:57:51 +0600

    forced user to give complex password

--- src/auth/dtos/LoginRequestDto.py
+++ src/auth/dtos/LoginRequestDto.py
@@ -1,7 +1,22 @@
 from dataclasses import dataclass
-from pydantic import constr
+from pydantic import constr, field_validator, EmailStr
+from config import Config
 
 @dataclass
 class LoginRequestDto:
-  email: constr(min_length=1) # type: ignore
+  email: EmailStr
   password: constr(min_length=1) # type: ignore
+
+  @field_validator("password")
+  def validatePassword(cls, password):
+    if len(password) < int(Config.getValByKey("PASSWORD_MAX_CHAR")):
+      raise ValueError(f"Password must be at least {Config.getValByKey("PASSWORD_MAX_CHAR")} characters long!")
+    if not any(char.isupper() for char in password):
+      raise ValueError("Password must contain at least one uppercase letter!")
+    if not any(char.islower() for char in password):
+      raise ValueError("Password must contain at least one lowercase letter!")
+    if not any(char.isdigit() for char in password):
+      raise ValueError("Password must contain at least one digit!")
+    if not any(char in "!@#$%^&*()" for char in password):
+      raise ValueError("Password must contain at least one special character!")
+    return password
--- src/user/dtos/UserCreateRequestDto.py
+++ src/user/dtos/UserCreateRequestDto.py
@@ -1,7 +1,22 @@
 from dataclasses import dataclass
-from pydantic import EmailStr, constr
+from pydantic import EmailStr, constr, field_validator
+from config import Config
 
 @dataclass
 class UserCreateRequestDto:
-  password: constr(min_length=1) # type: ignore
   email: EmailStr
+  password: constr(min_length=1) # type: ignore
+  
+  @field_validator("password")
+  def validatePassword(cls, password):
+    if len(password) < int(Config.getValByKey("PASSWORD_MAX_CHAR")):
+      raise ValueError(f"Password must be at least {Config.getValByKey("PASSWORD_MAX_CHAR")} characters long!")
+    if not any(char.isupper() for char in password):
+      raise ValueError("Password must contain at least one uppercase letter!")
+    if not any(char.islower() for char in password):
+      raise ValueError("Password must contain at least one lowercase letter!")
+    if not any(char.isdigit() for char in password):
+      raise ValueError("Password must contain at least one digit!")
+    if not any(char in "!@#$%^&*()" for char in password):
+      raise ValueError("Password must contain at least one special character!")
+    return password
