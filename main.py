6248e53570fb14b8c5eca75f8df0e54a2b2ce18b
Shahariar Kabir <maask5534@gmail.com>
2025-05-14 23:46:35 +0600

    CROS setup for local dev

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from src.auth import AuthRouter
 from src.auth.AuthMiddleware import AuthMiddleware
 from fastapi.security import HTTPBearer
 from typing import Annotated
+from fastapi.middleware.cors import CORSMiddleware
 
 def getEmail(email: Annotated[str, Header()]):
     return email
@@ -18,6 +19,19 @@ app.include_router(AuthRouter.routes)
 app.include_router(UserInsecureRouter.routes)
 
 app.add_middleware(AuthMiddleware)
+
+origins = [
+  "http://localhost:3000",  
+  "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,             
+  allow_credentials=True,            
+  allow_methods=["*"],               
+  allow_headers=["*"],               
+)
 
 @app.get("/",tags=["health"])
 async def test()->str:
