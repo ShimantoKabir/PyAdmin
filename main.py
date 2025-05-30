a7e569d74222f413ee7d4ca04036f621db564fc2
Shahariar Kabir <maask5534@gmail.com>
2025-05-30 17:07:40 +0600

    removed unnecessary code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, FastAPI, Header, BackgroundTasks
+from fastapi import Depends, FastAPI, Header
 from src.user import UserRouter
 from src.user import UserInsecureRouter
 from src.menu import MenuRouter
@@ -32,9 +32,6 @@ app.add_middleware(
   allow_methods=["*"],               
   allow_headers=["*"],               
 )
-
-def writeLog(message: str):
-  print(message)
 
 @app.get("/",tags=["health"])
 async def test()->str:
